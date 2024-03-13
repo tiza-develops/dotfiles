293b5f9e9287d58d9057522a494f6f68d4136caa
tiza <tizadevel@tuta.io>
2024-03-13 10:25:20 -0600

    corrected slight mistake with the curly braces

--- configuration.nix
+++ configuration.nix
@@ -131,6 +131,8 @@
 	      enable = true;
 	    };
 	  };
+	  };
+
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
